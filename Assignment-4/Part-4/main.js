a953ab46fb02a696c181eecdab63bf92ee5d4749
realgagansidhu <157822007+realgagansidhu@users.noreply.github.com>
2024-03-22 17:46:19 -0400

    final changes
    i made the final changes to file like collisiondetect and add the windoww keydown comments to the java file

--- Assignment-4/Part-4/main.js
+++ Assignment-4/Part-4/main.js
@@ -56,19 +56,19 @@ class Ball {
     this.y += this.velY;
   }
 
-  collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+collisionDetect() {
+  for (const ball of balls) {
+    if (!(this === ball) && ball.exists) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
-        }
+      if (distance < this.size + ball.size) {
+        ball.color = this.color = randomRGB();
       }
     }
   }
+}
 }
 
 const balls = [];
@@ -76,7 +76,7 @@ const balls = [];
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
+    
     // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
@@ -101,5 +101,21 @@ function loop() {
 
   requestAnimationFrame(loop);
 }
-
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "a":
+        this.x -= this.velX;
+        break;
+      case "d":
+        this.x += this.velX;
+        break;
+      case "w":
+        this.y -= this.velY;
+        break;
+      case "s":
+        this.y += this.velY;
+        break;
+    }
+  });
+  
 loop();
